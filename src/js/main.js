51d9259b89e8b3cc96d661191f097356f43b3260
AndresMarolt <andresmmarolt@gmail.com>
2022-02-08 22:14:34 -0300

    Desafío DOM

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,16 @@
-/* const { addListener } = require("nodemon"); */
-
 document.addEventListener('DOMContentLoaded', function() {
-    mostrarDinamicamente()
+    // mostrarDinamicamente();
+    alert("Dirigirse a Productos>Bajos y presionar Agregar al Carrito. Debajo de todo aparecerá la información del producto seleccionado");
+    agregarAlCarrito();
 });
 
-class ProdExpo {
-    constructor(nombre, precio, descripcion, imagen) {
+const carrito = [];
+
+class Producto {
+    constructor(nombre, precio, descripcion) {
         this.nombre = nombre;
         this.precio = precio;
         this.descripcion = descripcion;
-        this.imagen = imagen;
     }
 }
 
@@ -22,39 +23,39 @@ const articulosArray = [
     },
     {
         id: 2,
-        nombre: "Ernie Ball Musicman Stingray 5",
-        precio: 320000,
-        descripcion: "Mics hambuckers de alnico, previo activo con ecualizador de 3 bandas y cejuela compensada."
-    },
-    {
-        id: 3,
-        nombre: "Squier Classic Vibe 60s JB",
-        precio: 350000,
-        descripcion: "Jazz Bass inspirado en modelos Jazz Bass de los años 60. Cuerpo de álamo con acabado de poliuretano y diapasón de palorrosa"
-    },
-    {
-        id: 4,
-        nombre: "Fender Player JB",
-        precio: 120000,
-        descripcion: "Jazz Bass con cuerpo de aliso, diapasón de palorrosa y dos micrófonos Player Series de bobina simple"
-    },
-    {
-        id: 5,
         nombre: "Eastwood Classic Fretless Bass",
         precio: 70000,
         descripcion: "Bajo 4 cuerdas"
     },
     {
-        id: 6,
+        id: 3,
         nombre: "F Bass AC5 Violin Burst",
         precio: 500000,
         descripcion: "F Bass 5 cuerdas"
     },
     {
-        id: 7,
+        id: 4,
         nombre: "Warwick Rockbass Corvette 5",
         precio: 80000,
         descripcion: "Bajo 5 cuerdas"
+    },
+    {
+        id: 5,
+        nombre: "Fender Player JB",
+        precio: 120000,
+        descripcion: "Jazz Bass con cuerpo de aliso, diapasón de palorrosa y dos micrófonos Player Series de bobina simple"
+    },
+    {
+        id: 6,
+        nombre: "Ernie Ball Musicman Stingray 5",
+        precio: 320000,
+        descripcion: "Mics hambuckers de alnico, previo activo con ecualizador de 3 bandas y cejuela compensada."
+    },
+    {
+        id: 7,
+        nombre: "NS Design WAV4c",
+        precio: 30000,
+        descripcion: "Bajo 4 cuerdas"
     },
     {
         id: 8,
@@ -64,69 +65,120 @@ const articulosArray = [
     },
     {
         id: 9,
-        nombre: "NS Design WAV4c",
-        precio: 30000,
-        descripcion: "Bajo 4 cuerdas"
+        nombre: "Squier Classic Vibe 60s JB",
+        precio: 350000,
+        descripcion: "Jazz Bass inspirado en modelos Jazz Bass de los años 60. Cuerpo de álamo con acabado de poliuretano y diapasón de palorrosa"
     }
 ];
 
+function agregarAlCarrito() {
 
-function mostrarDinamicamente() {
-    const prods = document.getElementsByClassName("publicacion");
+    const enlaces = document.getElementsByClassName("publicacion");
 
-    for(const prod of prods) {
-        
-        prod.addEventListener('click', function(e) {
-            e.preventDefault();
+    console.log(enlaces);
 
-            console.log(prod.id);
-
-            const nroId = prod.id;
-
-            const prodEncontrado = articulosArray.find(x => x.id === nroId).id;
-
-            if(prodEncontrado) {
-                // =============== NOMBRE DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
-                let nombreHTML = document.createElement("h1");
-                nombreHTML.setAttribute("class", "producto__titulo");
-                nombreHTML.setAttribute("id", "nombre");
-                nombreHTML.innerText = prodEncontrado.nombre;
-
-                // =============== IMAGEN DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
-                let imagenProdHTML = document.createElement("img");
-                imagenProdHTML.setAttribute("src", "../src/img/bajos/destacado1.jpg");
-                imagenProdHTML.setAttribute("alt", "producto");
-                imagenProdHTML.setAttribute("class", "prod__imagen");
-
-                // =============== PRECIO DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
-                let precioHTML = document.createElement("p");
-                precioHTML.setAttribute("class", "prod__info__precio");
-                precioHTML.setAttribute("id", "precio");
-                precioHTML.innerText = prodEncontrado.precio;
-
-                // =============== DESCRIPCION DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
-                let descHTML = document.createElement("p")
-                descHTML.setAttribute("class", "prod__info__descripcion");
-                descHTML.setAttribute("id", "descripcion");
-                descHTML.innerText = prodEncontrado.descripcion;
-
-                // =============== DECLARAMOS LAS VARIABLES QUE CONTIENEN A LOS PADRES RESPECTIVOS ===============
-                let padreCont = document.querySelector("contenedor-prod");
-                let padreData = document.querySelector("prod-data");
-                let padreInfo = document.querySelector("prod-info");
-
-                
-
-                // =============== AÑADIMOS LOS ELEMENTOS CREADOS DE MANERA DINAMICA AL DOM ===============
-                padreCont.appendChild(nombreHTML);
-                padreData.appendChild(imagenProdHTML);
-                padreInfo.appendChild(precioHTML);
-                padreInfo.appendChild(descHTML);
-            }
+    for(const enlace of enlaces) {
+        enlace.addEventListener('click', evento => {
+            evento.preventDefault();
         })
     }
 
+    
+    const botones = document.getElementsByClassName("submit-carrito");
+
+    for(const boton of botones) {
+        boton.addEventListener('click', () => {
+            let publicacion = boton.parentElement.parentElement.parentElement.parentElement;
+        
+            let seleccionado = articulosArray.find(x => x.id == publicacion.id);
+
+            if(seleccionado) {
+
+                // =============== CREACION DEL OBJETO DE TIPO PRODUCTO PARA SUMARLO AL CARRITO ===============
+                const productoComprado = new Producto(seleccionado.nombre, seleccionado.precio, seleccionado.descripcion);
+                carrito.push(productoComprado);
+
+                // =============== NOMBRE DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
+                let nombreHTML = document.createElement("h1");
+                nombreHTML.innerText = productoComprado.nombre;
+
+                // =============== PRECIO DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
+                let precioHTML = document.createElement("p");
+                precioHTML.innerText = "$" + productoComprado.precio;
+
+                // =============== DESCRIPCION DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
+                let descHTML = document.createElement("p")
+                descHTML.innerText = productoComprado.descripcion;
+
+                // =============== TOTAL DE LA COMPRA HASTA EL MOMENTO DEL CLICK ===============
+                let montoCompra = carrito.reduce( (acumulador, elemento) => acumulador + elemento.precio, 0 );
+                let totalHTML = document.createElement("p");
+                totalHTML.innerText = montoCompra; 
+                
+                // =============== SELECCION DEL ELEMENTO PADRE AL QUE SE LE ANEXARAN LOS ELEMENTOS CREADOS ===============
+                let divPadre = document.getElementById("contenedor-bajos");
+                divPadre.appendChild(nombreHTML);
+                divPadre.appendChild(precioHTML);
+                divPadre.appendChild(descHTML);
+            }
+        })
+    }
 }
+
+// function mostrarDinamicamente() {
+//     const publicaciones = document.getElementsByClassName("publicacion");
+
+//     for(const prod of publicaciones) {
+        
+//         prod.addEventListener('click', e => {
+//             e.preventDefault();
+
+//             const prodEncontrado = articulosArray.find(x => x.id == prod.id);
+
+//             if(prodEncontrado) {
+//                 // =============== NOMBRE DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
+//                 let nombreHTML = document.createElement("h1");
+//                 nombreHTML.setAttribute("class", "producto__titulo");
+//                 nombreHTML.setAttribute("id", "nombre");
+//                 nombreHTML.innerText = prodEncontrado.nombre;
+
+//                 // =============== IMAGEN DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
+//                 let imagenProdHTML = document.createElement("img");
+//                 imagenProdHTML.setAttribute("src", prodEncontrado.imagen);
+//                 imagenProdHTML.setAttribute("alt", "producto");
+//                 imagenProdHTML.setAttribute("class", "prod__imagen");
+
+//                 // =============== PRECIO DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
+//                 let precioHTML = document.createElement("p");
+//                 precioHTML.setAttribute("class", "prod__info__precio");
+//                 precioHTML.setAttribute("id", "precio");
+//                 precioHTML.innerText = prodEncontrado.precio;
+
+//                 // =============== DESCRIPCION DEL PRODUCTO A MOSTRAR EN PANTALLA ===============
+//                 let descHTML = document.createElement("p")
+//                 descHTML.setAttribute("class", "prod__info__descripcion");
+//                 descHTML.setAttribute("id", "descripcion");
+//                 descHTML.innerText = prodEncontrado.descripcion;
+
+//                 miLocalStrg = window.localStorage;
+//                 miLocalStrg.setItem('nombreHTML', nombreHTML.innerHTML);
+//                 miLocalStrg.setItem('imagenProdHTMl', imagenProdHTML.innerHTML);
+//                 miLocalStrg.setItem('precioHTML', precioHTML.innerHTML);
+//                 miLocalStrg.setItem('descHTML', descHTML.innerHTML);
+//             }
+//         })
+//                  =============== DECLARAMOS LAS VARIABLES QUE CONTIENEN A LOS PADRES RESPECTIVOS ===============            
+//              const padreCont = document.querySelector('#contenedor-prod');
+//              const padreData = document.querySelector('#prod-data');
+//              const padreInfo = document.querySelector('#prod-info');
+            
+//              // =============== AÑADIMOS LOS ELEMENTOS CREADOS DE MANERA DINAMICA AL DOM ===============
+//              padreCont.appendChild(nombreHTML);
+//              padreData.appendChild(imagenProdHTML);
+//              padreInfo.appendChild(precioHTML);
+//              padreInfo.appendChild(descHTML);
+//     }
+// }
 
 
 
